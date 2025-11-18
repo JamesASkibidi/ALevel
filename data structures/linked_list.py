89b2b9af661b8a8e3c5c03d7dd2722107f8de244
JamesASkibidi <0243@balcarras.gloucs.sch.uk>
2025-11-18 12:28:32 +0000

    Finished basic programming

--- data structures/linked_list.py
+++ data structures/linked_list.py
@@ -8,6 +8,41 @@ def traverse():
 
 
 
+
+def add_item(item , linked_list):
+
+    global Next_Free
+    print(item)
+
+    if Next_Free != None:
+        linked_list[Next_Free]['data'] = item                            #set the next free's data as the new item
+        
+        NewFree = linked_list[Next_Free]['pointer']                      #store the pointer to the new next free location
+        
+        CurrentPointer = start
+
+        while CurrentPointer != None:                                    #loop though to find trhe final item
+            PrevPointer = CurrentPointer                                 #store the pointer of the final item
+            CurrentPointer = linked_list[CurrentPointer]['pointer']
+        
+        linked_list[PrevPointer]['pointer'] = Next_Free                  #point the final item to the new final item
+
+        linked_list[Next_Free]['pointer'] = None                         #point the new final item to null                   
+
+        Next_Free = NewFree                                              #set the next free location as the previous next frees pointer
+
+        return linked_list
+
+
+# def ()
+
+
+
+
+
+
+
+
 linked_list = [
     {'Data' : 'ketchup', 'pointer' : 1},        #0
     {'Data' : 'condiments', 'pointer' : 2},     #1
@@ -25,5 +60,20 @@ start = 0
 
 Next_Free = 5
 
-traverse()
 
+
+print('Original')
+print()
+traverse()
+print()
+print(start)
+print(Next_Free)
+
+linked_list = add_item('altitude' , linked_list)
+print()
+print('appended')
+print()
+traverse()
+print()
+print(start)
+print(Next_Free)
---
+++ data structures/playlist/playlistcreator.py
@@ -0,0 +1,22 @@
+import random
+
+def shuffler(queue):
+    .
+
+
+
+library = {
+    'Song Name' : 'location',
+    'Song 2' : '/playlist/library/Song2.mp3',
+    'Soul to squeeze' : '/playlist/library/Soul_To_Squeeze.mp3',
+    'Johnny B. Goode' : '/playlist/library/Johnny_B_Goode.mp3',
+    'Hurt' : '/playlist/library/Hurt.mp3',
+    'Tunnel Of Love' : '/playlist/library/Tunel_Of_Love',
+}
+
+queue = []
+head = -1
+tail = -1
+count=0
+
+NowPlaying = None
